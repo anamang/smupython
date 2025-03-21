c6a57648f6a535d9f288f992c133c858e3db7852
anamang <anamang@naver.com>
2025-03-21 18:19:14 +0900

    new file:   CircleCal.py 	new file:   inchTocm.py 	new file:   kgToPound.py 	new file:   test_python.py

---
+++ inchTocm.py
@@ -0,0 +1,3 @@
+print("inch를 cm로 변환하기")
+inch = float(input("inch> "))
+print(inch,"inch = ",inch*2.54)
---
+++ kgToPound.py
@@ -0,0 +1,3 @@
+print("kg를 로 lb변환하기")
+kg = float(input("kg> "))
+print(kg,"kg = 약",kg*2.2046)
