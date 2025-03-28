6a87ecbfa7e4e6250aba503f682f21cdf7f04822
anamang <anamang@naver.com>
2025-03-28 23:34:07 +0900

    modified:   w04.py

--- w04.py
+++ w04.py
@@ -9,3 +9,5 @@ elif number == 2 :
     number2_2 = int(input("두 번째 숫자를 입력하세요 :"))
     number2_3 = (number2_1 + number2_2)/2*(number2_2 - number2_1 +1)
     print(number2_1, "+...+", number2_2, "는",number2_3, "입니다")
+else :
+    print("1. 입력한 수식 계산 2.두 수 사이의 합계만 가능합니다")
