eeab58d197e24c1b77bc20d35037399b48e05231
anamang <anamang@naver.com>
2025-04-11 13:12:15 +0900

    modified:   python_test.py

--- python_test.py
+++ python_test.py
@@ -1,23 +1,12 @@
-coffee = 0
+def func1() :
+    a = 10
+    print("func1()에서 a값 %d" % a)
 
-def coffee_machine(button) :
-    print()
-    print("#1. 뜨거운 물을 준비한다.")
-    print("#2. 종이컵을 준비한다.")
+def func2() :
+    print("func2()에서 a값 %d" % a)
 
-    if coffee == 1 :
-        print("#3. 보통 커피를 탄다.")
-    elif coffee == 2 :
-        print("#3 설탕 커피를 탄다.")
-    elif coffee == 3 :
-        print("#3. 블랙 커피를 탄다.")
-    else :
-        print("#3. 아무 커피를 탄다.\n")
 
-    print("#4. 물을 붓는다.")
-    print("#5. 스푼으로 젓는다.")
-    print()
+a = 20
 
-coffee = int(input("어떤 커피 드릴까요?(1.보통,2:설탕,3:블랙)"))
-coffee_machine(coffee)
-print("손님~ 커피 여기있습니다.")
+func1()
+func2()
